397b566b681ef6b7d3b5f82de12674e9b848ebfa
Ayesh Karunaratne <ayesh@aye.sh>
2021-02-17 23:33:15 +0700

    [PHP 8.1] Conditional loading for CURLStringFile, and additional magic methods to account for mutability of CURLStringFile::$data property

--- src/Php81/Resources/stubs/CURLStringFile.php
+++ src/Php81/Resources/stubs/CURLStringFile.php
@@ -1,10 +1,37 @@
 <?php
 
-// https://php.watch/versions/8.1/CURLStringFile#CURLStringFile-polyfill
-class CURLStringFile extends CURLFile
-{
-    public function __construct(string $data, string $postname, string $mime = "application/octet-stream")
+if (extension_loaded('curl') && class_exists(CURLFile::class, false)) {
+    /**
+     * @property mixed $data
+     * https://php.watch/versions/8.1/CURLStringFile#CURLStringFile-polyfill
+     */
+    class CURLStringFile extends CURLFile
     {
-        parent::__construct('data://'. $mime .';base64,' . base64_encode($data), $mime, $postname);
+        private $data;
+
+        public function __construct(string $data, string $postname, string $mime = 'application/octet-stream')
+        {
+            $this->data = $data;
+            parent::__construct('data://' . $mime . ';base64,' . base64_encode($data), $mime, $postname);
+        }
+
+        public function __set(string $name, $value): void
+        {
+            if ('data' === $name) {
+                $this->name = 'data://application/octet-stream;base64,' . base64_encode($value);
+            }
+
+            $this->{$name} = $value;
+        }
+
+        public function __isset(string $name): bool
+        {
+            return isset($this->{$name});
+        }
+
+        public function __get(string $name)
+        {
+            return $this->{$name};
+        }
     }
 }
